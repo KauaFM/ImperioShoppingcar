6695bd49914ca744fa7b3fd452d51586228f700c
KauaFM <kkfelipemacedo@gmail.com>
2026-03-02 10:30:53 -0300

    Deploy limpo Imperio Shopping Car

---
+++ produtos.js
@@ -0,0 +1,13 @@
+const bancoDeProdutos = [
+    // ILUMINAÇÃO
+    { id: 1, categoria: 'iluminacao', nome: 'Farol Celta 01 Cromado', img: 'src/fundoNovo/farolCelta01...emdianteCromado.png' },
+    { id: 2, categoria: 'iluminacao', nome: 'H27 Super Branca', img: 'src/fundoNovo/h27superbranca1.png' },
+    
+    // EMBLEMAS (SHOPPING)
+    { id: 3, categoria: 'shopping', nome: 'Emblema Corcel 1', img: 'src/fundoNovo/emblemaCorcel1.png' },
+    { id: 4, categoria: 'shopping', nome: 'Emblema Grade Gol GVI', img: 'src/fundoNovo/emblemaGradeGolGVI.png' },
+    
+    // MECÂNICA
+    { id: 5, categoria: 'mecanica', nome: 'Bieleta Barra Onix', img: 'src/fundoNovo/bieletaBarraOnix.Orisma.png' },
+    // Adiciona os outros 180 aqui...
+];
